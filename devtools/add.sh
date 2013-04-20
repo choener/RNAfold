7859e5977d4abc1f374f0b003f348dd0f673b64e
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-04-20 15:56:27 +0200

    devtools / add.sh

---
+++ devtools/add.sh
@@ -0,0 +1,5 @@
+cabal-dev add-source ../Lib-PrimitiveArray
+cabal-dev add-source ../Lib-ADPfusion
+cabal-dev add-source ../Lib-BiobaseXNA
+cabal-dev add-source ../Lib-BiobaseTurner
+cabal-dev add-source ../Lib-BiobaseVienna
