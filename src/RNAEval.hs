acae083908c0b241af3e1fd3afc9489d112e0afa
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-04-20 22:38:30 +0200

    updated RNAFold and RNAEval

--- src/RNAEval.hs
+++ src/RNAEval.hs
@@ -1,22 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
--- | Simple wrapper around STrnafold
+-- | RNAEval tool.
 
 module Main where
 
+
+
+import System.Console.CmdArgs
+
+import Biobase.Vienna
 import BioInf.RNAfold
 import qualified Biobase.Turner.Import as TI
-import Biobase.Vienna
 
 
+
+data Options = Options
+  { params :: String
+  } deriving (Show,Data,Typeable)
+
+options = Options
+  { params = "./params" &= help "Turner 2004 RNA parameters (defaults to ./params)"
+  }
 
 main = do
+  Options{..} <- cmdArgs options
   xs <- fmap lines getContents
-  tm <- fmap turnerToVienna $ TI.fromDir "/home/choener/Documents/Workdata/TurnerRNA2004/RNA" "" ".dat"
+  tm <- fmap turnerToVienna $ TI.fromDir params "" ".dat"
   mapM_ (run' tm) $ toPairs xs
-
-test inp str = do
-  tm <- fmap turnerToVienna $ TI.fromDir "/home/choener/Documents/Workdata/TurnerRNA2004/RNA" "" ".dat"
-  run' tm (inp,str)
 
 toPairs (x1:x2:xs) = (x1,x2) : toPairs xs
 toPairs [x] = error "single last line remaining"
@@ -25,6 +36,12 @@ toPairs [] = []
 run' tm (inp,str) = do
   print $ length inp
   print $ rnaEval tm inp str
+
+
+
+test inp str = do
+  tm <- fmap turnerToVienna $ TI.fromDir "./params" "" ".dat"
+  run' tm (inp,str)
 
 tests = mapM_ (uncurry test)
   [ ( "CCUGACUGGCGUUGACAUAUGGUU"
@@ -53,11 +70,3 @@ tests = mapM_ (uncurry test)
     )
   ]
 
-
-{-
-doRNAfold inp = do
-  let (e,bs) = testRNAfold inp
-  putStrLn inp
-  print e
-  mapM_ putStrLn bs
--}
---
+++ src/RNAFold.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards #-}
+
+-- | Simple wrapper around the rnafold library.
+
+module Main where
+
+
+
+import System.Console.CmdArgs
+
+import BioInf.RNAfold
+import qualified Biobase.Turner.Import as TI
+import Biobase.Vienna
+
+
+
+data Options = Options
+  { params :: String
+  } deriving (Show,Data,Typeable)
+
+options = Options
+  { params = "./params" &= help "Turner 2004 RNA parameters (defaults to ./params)"
+  }
+
+main = do
+  Options{..} <- cmdArgs options
+  xs <- fmap lines getContents
+  tm <- fmap turnerToVienna $ TI.fromDir params "" ".dat"
+  mapM_ (run' tm) xs
+
+run' tm inp = do
+  print $ length inp
+  print $ rnaFold tm inp
+
