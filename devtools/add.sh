eb420e9c49a2a489112796e617de8c199c0155bf
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-04-22 03:32:09 +0200

    removed some libs from local devtools

--- devtools/add.sh
+++ devtools/add.sh
@@ -1,5 +1,1 @@
-cabal-dev add-source ../Lib-PrimitiveArray
 cabal-dev add-source ../Lib-ADPfusion
-cabal-dev add-source ../Lib-BiobaseXNA
-cabal-dev add-source ../Lib-BiobaseTurner
-cabal-dev add-source ../Lib-BiobaseVienna
